7060f6c19a4ac973fcd87fee697aa48cdc26cce6
rh137 <b06902137@csie.ntu.edu.tw>
2021-07-19 06:22:49 +0800

    feat: reset `chosen` when change question (by `useEffect`)

--- frontend/src/MyButton.js
+++ frontend/src/MyButton.js
@@ -1,13 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './MyButton.css';
 
-function MyButton ({ text, onClick, className }) {
+function MyButton ({ qid, text, onClick, className, useSetChosen }) {
   const [chosen, setChosen] = useState(false);
+
+  useEffect(() => {
+    setChosen(false);
+  }, [qid])
 
   return (
     <button className={className + " " + (chosen ? 'chosen' : 'not-chosen')}
             onClick={() => {
-              //setChosen(!chosen)
+              if (useSetChosen) setChosen(!chosen)
               onClick()
             }}>{text}</button>
   )
