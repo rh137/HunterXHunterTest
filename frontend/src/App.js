d37cc82c68398e104a03c8580087e937a1a304e7
rh137 <b06902137@csie.ntu.edu.tw>
2021-07-19 07:37:18 +0800

    feat: add info to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,18 @@ function App() {
 
   return (
     started ?
-      <QuestionPage /> :
-    <div className="App">
-      <h1>獵人官方念能力測驗</h1>
-      <Button type="primary" onClick={() => setStarted(true)}>開始</Button>
-    </div>
+      <QuestionPage restart={() => setStarted(false)}/> :
+    <>
+      <div className="App">
+        <h1>獵人官方念能力測驗</h1>
+        <Button type="primary" onClick={() => setStarted(true)}>開始</Button>
+      </div>
+      <div className="info">
+        <div><a href="https://www.dcard.tw/f/acg/p/226113580" target="_blank" rel="noopener noreferrer">Reference</a></div>
+        <div><a href="https://github.com/rh137" target="_blank" rel="noopener noreferrer">Program Author</a></div>
+        <div><a href="https://github.com/rh137/HunterXHunterTest" target="_blank" rel="noopener noreferrer">GitHub Repo</a></div>
+      </div>
+    </>
   )
 }
 
