1cf6e98f06910c3e2483cc1b15c8b1d39edd0ccd
rh137 <b06902137@csie.ntu.edu.tw>
2021-07-19 05:22:32 +0800

    feat: modify App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
+import React, { useState } from 'react';
 import { Button } from 'antd';
 import './App.css';
 
+import QuestionPage from "./QuestionPage";
+
 function App() {
+  const [started, setStarted] = useState(false);
+
   return (
+    started ?
+      <QuestionPage /> :
     <div className="App">
       <h1>獵人官方念能力測驗</h1>
-      <Button type="primary">開始</Button>
+      <Button type="primary" onClick={() => setStarted(true)}>開始</Button>
     </div>
-  );
+  )
 }
 
 export default App;
