4b72bf732eb37ac70431aa9c8e4127bf436d9a2f
rh137 <b06902137@csie.ntu.edu.tw>
2021-07-19 06:23:23 +0800

    feat: support multiple selection questions

--- frontend/src/QuestionPage.js
+++ frontend/src/QuestionPage.js
@@ -9,25 +9,60 @@ import MyButton from "./MyButton";
 function QuestionPage() {
   const [currentQuestionID, setCurrentQuestionID] = useState("q01");
   const [doneQuestionIDs, setDoneQuestionIDs] = useState([]);
-  const [choices, setChoices] = useState([]);
+  const [allQuestionChoices, setAllQuestionChoices] = useState([]);
+  const [currentQuestionChoices, setCurrentQuestionChoices] = useState([]);
+
 
   function renderQuestion(id) {
+    if (id === "END") {
+      return <><h1>END</h1></>
+    }
     const q = questions[id];
-    return (
+    return q.isSingleSelection ? (
       <>
         <h1>{q.header}</h1>
         <h1>{q.question}</h1>
         {q.options.map(opt => {
           return <div>
-            <MyButton text={opt.id + '. ' + opt.body}
+            <MyButton qid={id}
+                      text={opt.id + '. ' + opt.body}
                       className="option"
                       onClick={() => {
                         setDoneQuestionIDs([...doneQuestionIDs, currentQuestionID]);
                         setCurrentQuestionID(opt.next ? opt.next : q.next);
-                        setChoices([...choices, opt.id])
+                        setAllQuestionChoices([...allQuestionChoices, opt.id])
                       }}/>
           </div>
         })}
+      </>
+    ) : (
+      <>
+        <h1>{q.header}</h1>
+        <h1>{q.question}</h1>
+        {q.options.map(opt => {
+          return <div>
+            <MyButton qid={id}
+                      text={opt.id + '. ' + opt.body}
+                      className="option"
+                      useSetChosen={true}
+                      onClick={() => {
+                        const index = currentQuestionChoices.indexOf(opt.id);
+                        let newChoices = Array.from(currentQuestionChoices);
+                        if (index !== -1)
+                          newChoices.splice(index, 1)
+                        else
+                          newChoices.push(opt.id);
+                        setCurrentQuestionChoices(newChoices);
+                      }}/>
+          </div>
+        })}
+        <MyButton text="確認" className="option"
+                  onClick={() => {
+                    setDoneQuestionIDs([...doneQuestionIDs, currentQuestionID]);
+                    setCurrentQuestionID(q.next);
+                    setAllQuestionChoices([...allQuestionChoices, currentQuestionChoices])
+                    setCurrentQuestionChoices([])
+                  }} />
       </>
     )
   }
@@ -38,9 +73,10 @@ function QuestionPage() {
               onClick={() => {
                 setCurrentQuestionID(doneQuestionIDs[doneQuestionIDs.length - 1]);
                 setDoneQuestionIDs(doneQuestionIDs.slice(0, doneQuestionIDs.length - 1));
-                setChoices(choices.slice(0, choices.length - 1))
+                setAllQuestionChoices(allQuestionChoices.slice(0, allQuestionChoices.length - 1));
+                setCurrentQuestionChoices([]);
               }} disabled={currentQuestionID === 'q01'}>返回上一題</Button>
-      <Button onClick={() => {alert(choices); alert(doneQuestionIDs)}}>check</Button>
+      <Button onClick={() => {console.log(allQuestionChoices); alert(doneQuestionIDs)}}>check</Button>
       <div className="questionBlock">
         {renderQuestion(currentQuestionID)}
       </div>
